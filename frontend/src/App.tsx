176e209124b7b405605e47b23ee03a1745cda737
kbrah-gen5 <k.brahma.nirvana@gmail.com>
2025-03-10 23:12:34 +0900

    front なんとなく動く

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/Login';
 import PostList from './components/PostList';
 import MyPosts from './components/MyPosts';
@@ -11,9 +11,13 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/posts" component={PostList} />
         <Route path="/myposts" component={MyPosts} />
+        {/* Add a default redirect to one of your routes */}
+        <Route exact path="/">
+          <Redirect to="/posts" />
+        </Route>
       </Switch>
     </Router>
   );
 }
 
-export default App; 
+export default App;
