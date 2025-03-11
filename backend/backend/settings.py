a5793f1b9462a170abcd48ea2815bbf4282408c5
kbrah-gen5 <k.brahma.nirvana@gmail.com>
2025-03-11 15:35:28 +0900

    secret key into .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-..."
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
